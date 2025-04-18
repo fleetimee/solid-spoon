cb282130f3b6e75807319c9d743ff888bd7a08a3
Novian Andika <novian.andika@students.amikom.ac.id>
2025-04-18 14:21:30 +0700

    feat: add nextCookies plugin to betterAuth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 import { admin } from "better-auth/plugins";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
   }),
-  plugins: [admin()],
+  plugins: [admin(), nextCookies()],
   emailAndPassword: {
     enabled: true,
   },
