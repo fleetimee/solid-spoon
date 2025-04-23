450ed1be2289d847a4fa6d303f5ec5e2c0cb8f91
Novian Andika <novian.andika@students.amikom.ac.id>
2025-04-23 13:17:40 +0700

    feat: update ESLint configuration to import settings from .eslintrc.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  FlatCompat
+    .importEslintrc("./.eslintrc.json")
+    .then((config) => {
+      return compat.config(config);
+    })
+    .catch((error) => {
+      console.error("Error importing ESLint config:", error);
+      return {};
+    }),
+  {
+    files: ["*.ts", "*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
