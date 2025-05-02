80b4ded90221d89d9dbc82cd65e8019d125db854
Novian Andika <novian.andika@students.amikom.ac.id>
2025-05-02 23:25:21 +0700

    feat: implement password reset functionality; add email sending logic with Resend integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,11 @@ import { Pool } from "pg";
 import { admin } from "better-auth/plugins";
 import { nextCookies } from "better-auth/next-js";
 
+import { Resend } from "resend";
+import { EmailTemplate } from "@daveyplate/better-auth-ui/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -12,5 +17,26 @@ export const auth = betterAuth({
     enabled: true,
     disableSignUp: true,
     autoSignIn: false,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      const name = user.name || user.email.split("@")[0];
+
+      console.log("Sending password reset email to:", user.email);
+
+      await resend.emails.send({
+        from: "CapstoneD <capstone-kelompok-d@capstone-mail.fleetime.my.id>",
+        to: user.email,
+        subject: "Reset Your Password",
+        react: EmailTemplate({
+          action: "Reset Password",
+          heading: "Reset Your Password",
+          content: `Hello ${name}, please reset your password by clicking the link below.`,
+          siteName: "NEW-TECH",
+          baseUrl: "https://newtech.dev",
+          url,
+        }),
+
+        text: `Hello ${name},\n\nPlease reset your password by clicking the link below:\n\n${url}\n\nBest regards,\nYour Team`,
+      });
+    },
   },
 });
